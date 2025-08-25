88746be991de9784e026e272c050df9478e16a51
Yuvraj <Yuvi724@gmail.com>
2025-08-25 18:41:34 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  "projectId": "chronoflow-gi9hc",
+  "appId": "1:147748348027:web:e47f7ef6c0a75aa67342de",
+  "storageBucket": "chronoflow-gi9hc.firebasestorage.app",
+  "apiKey": "AIzaSyBxibxrL-N1qSqpomHUv7nYsq5ju4Acnm4",
+  "authDomain": "chronoflow-gi9hc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "147748348027"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
